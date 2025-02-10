8c01941b3ac008c85d296eb7803d224a81a5798c
Rommel Martínez <ebzzry@icloud.com>
2025-02-10 23:59:27 +0800

    [top-level] clean up

--- src/etc.lisp
+++ src/etc.lisp
@@ -12,9 +12,7 @@
 (define-handler build ("build"))
 (define-command nil build ()
   "build a derivation or fetch a store path"
-  "[-n]"
-  (build/options)
-  #'build/handler
+  "[-n]" t t
   "Build the default package from the flake in the current directory"
   "vix build"
   "Build `hello' and `cowsay' from Nixpkgs, leaving two result symlinks"
@@ -24,9 +22,7 @@
 (define-handler run ("run"))
 (define-command nil run ()
   "run a Nix application"
-  nil
-  (run/options)
-  #'run/handler
+  nil t t
   "Run `vim' from the `nixpkgs' flake"
   "vix run -n vim")
 
@@ -34,17 +30,13 @@
 (define-handler bundle ("bundle"))
 (define-command nil bundle ()
   "bundle an application so that it works outside of the Nix store"
-  nil
-  (bundle/options)
-  #'bundle/handler
+  nil t t
   "Bundle `hello'"
   "vix bundle -n vim")
 
 (define-command nil copy ()
   "start an interactive environment for evaluating Nix expressions"
-  nil
-  nil
-  nil
+  nil nil nil
   "Copy all store paths from a local binary cache"
   "vix copy --all --from file:///tmp/cache")
 
@@ -52,24 +44,24 @@
 (define-handler edit ("edit"))
 (define-command nil edit ()
   "open the Nix expression of a Nix package in $EDITOR"
-  nil
-  (edit/options)
-  #'edit/handler
+  nil t t
   "Open the Nix expression of the `hello' package"
   "vix edit -n hello")
 
 (define-command nil eval ()
   "evaluate a Nix expression"
-  nil
-  nil
-  nil
+  nil nil nil
   "Evaluate a Nix expression given on the command line"
   "vix eval --expr '1 + 2'")
 
 (define-command nil fmt (format)
   "reformat your code in the standard style"
-  nil
-  nil
-  nil
+  nil nil nil
   "Format the current flake"
   "vix fmt")
+
+(define-command nil repl ()
+  "start an interactive environment for evaluating Nix expressions"
+  nil nil nil
+  "Evaluate some simple Nix expressions"
+  "vix repl")
--- src/flake.lisp
+++ src/flake.lisp
@@ -10,25 +10,19 @@
 
 (define-command flake init (fi)
   "create a flake in the current directory"
-  nil
-  nil
-  nil
+  nil nil nil
   "Create a flake using the default template"
   "vix init")
 
 (define-command flake metadata (fm)
   "show flake metadata"
-  nil
-  nil
-  nil
+  nil nil nil
   "Show flake metadata"
   "vix meta")
 
 (define-command flake show (fs)
   "show the outputs provided by a flake"
-  nil
-  nil
-  nil
+  nil nil nil
   "Show the output attributes provided by the CWD flake"
   "vix show"
   "List available templates"
@@ -36,51 +30,38 @@
 
 (define-command flake update (fu)
   "update flake lock file"
-  nil
-  nil
-  nil
+  nil nil nil
   "Update all inputs"
   "vix update")
 
 (define-command flake new (fn)
   "create a flake in the specified directory from a template"
-  nil
-  nil
-  nil
+  nil nil nil
   "Create a flake in the directory hello"
   "vix new hello"
   "Create a flake in the directory hello using template haskell-hello"
-  "vix new hello -t templates#haskell-hello"
-  )
+  "vix new hello -t templates#haskell-hello")
 
 (define-command flake clone (fc)
   "clone flake repository"
-  nil
-  nil
-  nil
+  nil nil nil
   "Check out the source code of the dwarffs flake"
   "vix clone dwarffs --dest dwarffs")
 
 (define-command flake check (fh)
   "check whether the flake evaluates and run its tests"
-  nil
-  nil
-  nil
+  nil nil nil
   "Evaluate the flake in the current directory, and build its checks"
   "vix check")
 
 (define-command flake archive (fa)
   "copy a flake and all its inputs to a store"
-  nil
-  nil
-  nil
+  nil nil nil
   "Fetch the dwarffs flake to the local Nix store"
   "vix achive dwarffs")
 
 (define-command flake prefetch (fp)
   "download the flake source tree into the Nix store"
-  nil
-  nil
-  nil
+  nil nil nil
   "Download the dwarffs flake"
   "vix prefetch dwarffs --json")
