09f6d72039e81494d0c0cc2f2842650f77ee137f
Rommel Martínez <ebzzry@icloud.com>
2025-02-15 19:56:53 +0800

    [flake] refactor `init` command

--- src/flake.lisp
+++ src/flake.lisp
@@ -8,11 +8,50 @@
 
 (in-package #:vix/src/flake)
 
+(def- flake/init/handler (cmd)
+  "Define a function for handling the `flake init' command."
+  (let ((args (clingon:command-arguments cmd)))
+    (with-output-file (out #P"flake.nix")
+      (format out "
+{
+  description = \"A flake\";
+  inputs = {
+    nixpkgs.url = \"github:nixos/nixpkgs/nixpkgs-unstable\";
+    flake-utils.url = \"github:numtide/flake-utils\";
+  };
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        apps = import ./apps.nix { inherit pkgs; };
+        devShells = import ./shells.nix { inherit nixpkgs pkgs; };
+      });
+}
+"))
+    (with-output-file (out #P"apps.nix")
+      (format out "
+{ pkgs }: rec {
+  hello = {
+    type = \"app\";
+    program = \"${pkgs.hello}/bin/hello\";
+  };
+  default = hello;
+}
+"))
+    (with-output-file (out #P"shells.nix")
+      (format out "
+{ nixpkgs, pkgs, ... }:
+with pkgs; rec {
+  hello = mkShell { buildInputs = [ pkgs.hello ]; };
+  default = hello;
+}
+"))))
+
 (define-command flake init (i)
   "create a flake in the current directory"
   ""
   nil
-  t
+  #'flake/init/handler
   nil
   "Create a flake using the default template"
   "f i")
