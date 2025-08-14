d5b0d696c0d9cac19f7cd372bccc2600c6b4f135
Rommel Martínez <ebzzry@icloud.com>
2025-08-14 19:20:18 +0800

    [shells.nix] add zstd

--- flake.nix
+++ flake.nix
@@ -4,8 +4,19 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { devShells = import ./shells.nix { inherit nixpkgs pkgs; }; });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = import ./shells.nix { inherit nixpkgs pkgs; };
+      }
+    );
 }
--- shells.nix
+++ shells.nix
@@ -1,5 +1,41 @@
-{ nixpkgs, pkgs, ... }:
-with pkgs; rec {
-  lisp = mkShell { buildInputs = [ sbcl ecl ]; };
+{
+  nixpkgs,
+  pkgs,
+  ...
+}:
+with pkgs;
+let
+  comDeps = [
+    which
+    rlwrap
+    file
+    getopt
+    glib
+    openssl
+    zlib
+    zstd
+  ];
+  extraLibs = [
+    libGL
+    libGLU
+    meson
+    glfw
+    zstd
+  ];
+  addComDeps = list: list ++ comDeps;
+in
+rec {
+  lisp = mkShell {
+    buildInputs =
+      addComDeps [
+        sbcl
+        ecl
+        cl-launch
+        libfixposix
+      ]
+      ++ extraLibs;
+    LD_LIBRARY_PATH = nixpkgs.lib.strings.makeLibraryPath extraLibs;
+  };
+  cl = lisp;
   default = lisp;
 }
